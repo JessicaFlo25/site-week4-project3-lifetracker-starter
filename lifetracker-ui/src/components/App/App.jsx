c7cb029f726c6741441293ebd4344d5a1bef6ad6
JessicaFlo25 <jessica.flores309@myhunter.cuny.edu>
2023-07-03 14:41:38 -0700

    Implemented Login Form

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -1,31 +1,101 @@
+import React from 'react'
 import { useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import Home from "../Home/Home"
 import Navbar from "../Navbar/Navbar"
-// import Register from "../Register/Register"
-// import Login from "../Login/Login"
-// import Portal from "../Portal/Portal"
-import './App.css'
+import Landing from "../Landing/Landing"
+import LoginPage from "../LoginPage/LoginPage"
+import RegistrationPage from "../RegistrationPage/RegistrationPage"
+import ActivityPage from "../ActivityPage/ActivityPage"
+import NutritionPage from "../NutritionPage/NutritionPage"
+import NotFound from '../NotFound/NotFound'
+import "./App.css"
 
 
-function App() {
-  const[appState,setAppState] = useState({})
+
+const App = () => {
+
+  const [loggedIn, setLoggedIn] = useState(false);//will need later to know when and not to display login/register buttons
+  const [loginError, setLoginError] = useState("");
+  const [userName, setUserName] = useState();
+
+
+  const handleLogin = async (email, password) => {
+    try {
+      const response = await fetch("http://localhost:3001/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        //Successful Login
+        setLoggedIn(true);
+        setLoginError("");
+        console.log(data.message); //optional - display a success message
+      } else {
+        //Login failed
+        setLoginError(data.message);
+        console.log(data.message); //optional - display error message
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  //Registration function to handle registration
+  const handleRegistration = async (name, email, password) => {
+    try {
+      const response = await fetch("http://localhost:3001/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+
+      //wait for the response
+      const data = await response.json();
+
+      if (response.ok) {
+        //Registration successful
+        setLoggedIn(true);
+        console.log(data.message); //optional - display a success message
+      } else {
+        //Registration failed
+        console.log(data.message); //optional - display error meesage
+      }
+    } catch (error) {
+      console.error("Error: ", error);
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
 
   return (
     <div className='app'>
       <BrowserRouter>
-      <Navbar/>
+      <Navbar loggedIn={loggedIn}/> 
+      {/* above passes t/f and will affect what buttons appear */}
         <Routes>
-          {/* <Route path="/" element={<Home />}/> */}
-          {/* <Route path="/register" element={<Register />}/>
-          <Route path="/login" element={<Login />}/> */}
-          {/* display the components conditionally */}
+          <Route path="/" element={<Landing/>}/> 
+          <Route path="/login" element={<LoginPage onLogin={handleLogin} error={loginError} />}/>
+          <Route path="/register" element={<RegistrationPage/>}/>  
+          {/* conditional rendering, need to figure out a conditional using a state and make sure wildcard is last conditional */}
+          <Route path="/activity" element={<ActivityPage />}/>
+          <Route path="/nutrtion/*" element={<NutritionPage />}/>
+          <Route path="*" element={<NotFound/>}/>
+          
         </Routes>
+      
+      
       </BrowserRouter>
-
-
-
-
     </div>
   )
 }
--- lifetracker-ui/src/components/Navbar/Navbar.jsx
+++ lifetracker-ui/src/components/Navbar/Navbar.jsx
@@ -1,19 +1,52 @@
-// import React from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./Navbar.css"
 
-// const Navbar = () => {
-//   return (
-//     <div>
-//         <nav className='nav'>
-//             <a href = "/">
-//                 <img src ="https://images.app.goo.gl/QN1NtW55WtFXbK5a6"/>
-//             </a>
-//             <li><Link to="/">Home</Link></li>
-//             <li></li>
-//             <li></li>
+const Navbar = ({loggedIn}) => {
+  return (
+    <div>
+        <nav className='Navbar'>
+            <ul className='listedthings'>
+            <li>
+                <Link to="/">
+                    <img className='logo' src="https://live.staticflickr.com/1262/536422340_93e740dc28_c.jpg" alt='logo'/>
+                </Link>
+            </li>
+            <li>
+                <Link to="/activity"><p>Activity</p></Link>
+            </li>
+            <li>
+                <Link to="/exercise"><p>Exercise</p></Link>
+            </li>
+            <li>
+                <Link to="/nutrition"><p>Nutrition</p></Link>
+            </li>
+            <li>
+                <Link to="/sleep"><p>Sleep</p></Link>
+            </li>
+            </ul>
+            {
+                loggedIn ? (
+                    <ul>
+                    <button className='SignOutBttn'>Sign Out</button>
+                     </ul>
+                ): (
+                    <ul>
+                        <li className='buttons'>
+                            <Link to="/login">
+                            <button className="Loginbttn">Login</button>
+                            </Link>
+                            <Link to="/register">
+                            <button className='Registerbttn'>Register</button>
+                            </Link>
+                        </li>
+                    </ul>
+                )
+            }
 
-//         </nav>
-//     </div>
-//   )
-// }
+        </nav>
+    </div>
+)}
 
-// export default Navbar
+export default Navbar
+//                         
